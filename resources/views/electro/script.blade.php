718630f0b79e9adb33364b960a3da92e8180505c
Adan Nugraha <contact.ardana@gmail.com>
2022-01-09 19:59:56 +0700

    templating blade

---
+++ resources/views/electro/script.blade.php
@@ -0,0 +1,7 @@
+<!-- jQuery Plugins -->
+		<script src="{{ asset('electro/js/jquery.min.js') }}"></script>
+		<script src="{{ asset('electro/js/bootstrap.min.js') }}"></script>
+		<script src="{{ asset('electro/js/slick.min.js') }}"></script>
+		<script src="{{ asset('electro/js/nouislider.min.js') }}"></script>
+		<script src="{{ asset('electro/js/jquery.zoom.min.js') }}"></script>
+		<script src="{{ asset('electro/js/main.js') }}"></script>
